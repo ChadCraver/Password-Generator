f640366604fa257199f0d60d5dcdb7b8aac148e8
Chad Craver <craver.chad@gmail.com>
2018-09-25 15:02:47 -0400

    Add project files.

---
+++ Password Generator/frmMain.Designer.vb
@@ -0,0 +1,166 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtOutput = New System.Windows.Forms.TextBox()
+        Me.lblMaxChars = New System.Windows.Forms.Label()
+        Me.spinMaxChars = New System.Windows.Forms.NumericUpDown()
+        Me.lblExclude = New System.Windows.Forms.Label()
+        Me.txtExclude = New System.Windows.Forms.TextBox()
+        Me.btnGenerate = New System.Windows.Forms.Button()
+        Me.lblContainsSpecialChar = New System.Windows.Forms.Label()
+        Me.lblContainsNumber = New System.Windows.Forms.Label()
+        Me.lblContainsLower = New System.Windows.Forms.Label()
+        Me.lblContainsUpper = New System.Windows.Forms.Label()
+        CType(Me.spinMaxChars, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'txtOutput
+        '
+        Me.txtOutput.Location = New System.Drawing.Point(11, 124)
+        Me.txtOutput.Name = "txtOutput"
+        Me.txtOutput.Size = New System.Drawing.Size(322, 20)
+        Me.txtOutput.TabIndex = 3
+        '
+        'lblMaxChars
+        '
+        Me.lblMaxChars.AutoSize = True
+        Me.lblMaxChars.Location = New System.Drawing.Point(9, 24)
+        Me.lblMaxChars.Name = "lblMaxChars"
+        Me.lblMaxChars.Size = New System.Drawing.Size(108, 13)
+        Me.lblMaxChars.TabIndex = 1
+        Me.lblMaxChars.Text = "Maximum Characters:"
+        '
+        'spinMaxChars
+        '
+        Me.spinMaxChars.Location = New System.Drawing.Point(123, 22)
+        Me.spinMaxChars.Maximum = New Decimal(New Integer() {25, 0, 0, 0})
+        Me.spinMaxChars.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.spinMaxChars.Name = "spinMaxChars"
+        Me.spinMaxChars.Size = New System.Drawing.Size(59, 20)
+        Me.spinMaxChars.TabIndex = 0
+        Me.spinMaxChars.Value = New Decimal(New Integer() {12, 0, 0, 0})
+        '
+        'lblExclude
+        '
+        Me.lblExclude.AutoSize = True
+        Me.lblExclude.Location = New System.Drawing.Point(40, 48)
+        Me.lblExclude.Name = "lblExclude"
+        Me.lblExclude.Size = New System.Drawing.Size(77, 13)
+        Me.lblExclude.TabIndex = 3
+        Me.lblExclude.Text = "Exclude these:"
+        '
+        'txtExclude
+        '
+        Me.txtExclude.Location = New System.Drawing.Point(123, 45)
+        Me.txtExclude.Name = "txtExclude"
+        Me.txtExclude.Size = New System.Drawing.Size(210, 20)
+        Me.txtExclude.TabIndex = 1
+        '
+        'btnGenerate
+        '
+        Me.btnGenerate.Location = New System.Drawing.Point(135, 84)
+        Me.btnGenerate.Name = "btnGenerate"
+        Me.btnGenerate.Size = New System.Drawing.Size(75, 23)
+        Me.btnGenerate.TabIndex = 2
+        Me.btnGenerate.Text = "Generate"
+        Me.btnGenerate.UseVisualStyleBackColor = True
+        '
+        'lblContainsSpecialChar
+        '
+        Me.lblContainsSpecialChar.AutoSize = True
+        Me.lblContainsSpecialChar.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblContainsSpecialChar.ForeColor = System.Drawing.Color.Red
+        Me.lblContainsSpecialChar.Location = New System.Drawing.Point(40, 169)
+        Me.lblContainsSpecialChar.Name = "lblContainsSpecialChar"
+        Me.lblContainsSpecialChar.Size = New System.Drawing.Size(263, 15)
+        Me.lblContainsSpecialChar.TabIndex = 4
+        Me.lblContainsSpecialChar.Text = "Password does not contain a special character."
+        '
+        'lblContainsNumber
+        '
+        Me.lblContainsNumber.AutoSize = True
+        Me.lblContainsNumber.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblContainsNumber.ForeColor = System.Drawing.Color.Red
+        Me.lblContainsNumber.Location = New System.Drawing.Point(40, 191)
+        Me.lblContainsNumber.Name = "lblContainsNumber"
+        Me.lblContainsNumber.Size = New System.Drawing.Size(213, 15)
+        Me.lblContainsNumber.TabIndex = 4
+        Me.lblContainsNumber.Text = "Password does not contain a number."
+        '
+        'lblContainsLower
+        '
+        Me.lblContainsLower.AutoSize = True
+        Me.lblContainsLower.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblContainsLower.ForeColor = System.Drawing.Color.Red
+        Me.lblContainsLower.Location = New System.Drawing.Point(40, 213)
+        Me.lblContainsLower.Name = "lblContainsLower"
+        Me.lblContainsLower.Size = New System.Drawing.Size(259, 15)
+        Me.lblContainsLower.TabIndex = 4
+        Me.lblContainsLower.Text = "Password does not contain a lower case letter."
+        '
+        'lblContainsUpper
+        '
+        Me.lblContainsUpper.AutoSize = True
+        Me.lblContainsUpper.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblContainsUpper.ForeColor = System.Drawing.Color.Red
+        Me.lblContainsUpper.Location = New System.Drawing.Point(40, 235)
+        Me.lblContainsUpper.Name = "lblContainsUpper"
+        Me.lblContainsUpper.Size = New System.Drawing.Size(268, 15)
+        Me.lblContainsUpper.TabIndex = 4
+        Me.lblContainsUpper.Text = "Password does not contain an upper case letter."
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(345, 288)
+        Me.Controls.Add(Me.lblContainsUpper)
+        Me.Controls.Add(Me.lblContainsLower)
+        Me.Controls.Add(Me.lblContainsNumber)
+        Me.Controls.Add(Me.lblContainsSpecialChar)
+        Me.Controls.Add(Me.btnGenerate)
+        Me.Controls.Add(Me.txtExclude)
+        Me.Controls.Add(Me.lblExclude)
+        Me.Controls.Add(Me.spinMaxChars)
+        Me.Controls.Add(Me.lblMaxChars)
+        Me.Controls.Add(Me.txtOutput)
+        Me.Name = "frmMain"
+        Me.Text = "Password Generator"
+        CType(Me.spinMaxChars, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtOutput As TextBox
+    Friend WithEvents lblMaxChars As Label
+    Friend WithEvents spinMaxChars As NumericUpDown
+    Friend WithEvents lblExclude As Label
+    Friend WithEvents txtExclude As TextBox
+    Friend WithEvents btnGenerate As Button
+    Friend WithEvents lblContainsSpecialChar As Label
+    Friend WithEvents lblContainsNumber As Label
+    Friend WithEvents lblContainsLower As Label
+    Friend WithEvents lblContainsUpper As Label
+End Class
---
+++ Password Generator/frmMain.vb
@@ -0,0 +1,133 @@
+﻿Imports System.ComponentModel
+
+Public Class frmMain
+    Public Generator As System.Random
+
+    Private Enum CharType
+        Number = 1
+        Letter = 2
+        SpecialChar = 3
+    End Enum
+
+    Private Sub btnGenerate_Click(sender As Object, e As EventArgs) Handles btnGenerate.Click
+        Do
+            txtOutput.Text = GeneratePassword()
+        Loop Until CheckPasswordStrength(txtOutput.Text) = True
+    End Sub
+
+    Private Function CheckPasswordStrength(ByVal Password As String) As Boolean
+        Dim arrPassword() As Char = Password.ToCharArray
+        Dim ContainsSpecialChar As Boolean = False
+        Dim ContainsNumber As Boolean = False
+        Dim ContainsLowerChar As Boolean = False
+        Dim ContainsUpperChar As Boolean = False
+        Dim Numbers() As String = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}
+        Dim LCaseLetters() As String = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
+        Dim UCaseLetters() As String = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
+        Dim SpecialChars() As String = {"!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "`", "~", "[", "]", "{", "}", ":", ";", "?", ".", ",", "\", "/", "|", "<", ">"}
+
+        For X = 0 To arrPassword.Length - 1
+            ' Check if password contains a number.
+            If Numbers.Contains(arrPassword(X)) Then
+                ContainsNumber = True
+            End If
+            ' Check if password contains a special character.
+            If SpecialChars.Contains(arrPassword(X)) Then
+                ContainsSpecialChar = True
+                Exit For
+            End If
+            ' Check if password contains a lowercase character.
+            If LCaseLetters.Contains(arrPassword(X)) Then
+                ContainsLowerChar = True
+            End If
+            ' Check if password contains an uppercase character.
+            If UCaseLetters.Contains(arrPassword(X)) Then
+                ContainsUpperChar = True
+            End If
+        Next
+
+        If ContainsSpecialChar Then
+            lblContainsSpecialChar.Text = "Password contains a special character."
+            lblContainsSpecialChar.ForeColor = Color.Green
+        Else
+            lblContainsSpecialChar.Text = "Password does not contain a special character."
+            lblContainsSpecialChar.ForeColor = Color.Red
+        End If
+
+        If ContainsNumber Then
+            lblContainsNumber.Text = "Password contains a number."
+            lblContainsNumber.ForeColor = Color.Green
+        Else
+            lblContainsNumber.Text = "Password does not contain a number."
+            lblContainsNumber.ForeColor = Color.Red
+        End If
+
+        If ContainsLowerChar Then
+            lblContainsLower.Text = "Password contains a lower case character."
+            lblContainsLower.ForeColor = Color.Green
+        Else
+            lblContainsLower.Text = "Password does not contain a lower case character."
+            lblContainsLower.ForeColor = Color.Red
+        End If
+
+        If ContainsUpperChar Then
+            lblContainsUpper.Text = "Password contains an upper case character."
+            lblContainsUpper.ForeColor = Color.Green
+        Else
+            lblContainsUpper.Text = "Password does not contain an upper case character."
+            lblContainsUpper.ForeColor = Color.Red
+        End If
+
+        If ContainsNumber And ContainsSpecialChar And ContainsLowerChar And ContainsUpperChar Then
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+
+    Private Function GeneratePassword() As String
+        Dim intLength As Integer = spinMaxChars.Value
+        Dim arrExcludeChars() As Char = txtExclude.Text.ToCharArray
+        Dim outputPassword As String = ""
+        Dim strNewChar As Char = ""
+        Generator = New System.Random
+
+        For i = 1 To intLength
+            Do
+                strNewChar = GetRandomChar(Generator.Next(1, 4))
+            Loop Until Not arrExcludeChars.Contains(strNewChar)
+
+            outputPassword &= strNewChar
+        Next
+
+        Return outputPassword
+    End Function
+
+    Private Function GetRandomChar(ByVal CharType As CharType) As String
+        Dim intRandom As Integer = 0
+
+        Select Case CharType
+            Case CharType.Number
+                Dim Numbers() As Integer = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+                intRandom = Generator.Next(0, Numbers.Length)
+                Return Numbers(intRandom)
+            Case CharType.Letter
+                Dim Letters() As String = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
+                intRandom = Generator.Next(0, Letters.Length)
+                Select Case Generator.Next(1, 3)
+                    Case 1
+                        Return Letters(intRandom)
+                    Case 2
+                        Return UCase(Letters(intRandom))
+                    Case Else
+                        Return UCase(Letters(intRandom))
+                End Select
+            Case CharType.SpecialChar
+                Dim SpecialChars() As String = {"!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "`", "~", "[", "]", "{", "}", ":", ";", "?", ".", ",", "\", "/", "|", "<", ">"}
+                intRandom = Generator.Next(0, SpecialChars.Length)
+                Return SpecialChars(intRandom)
+        End Select
+
+        Return ""
+    End Function
+End Class
